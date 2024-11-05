b8611f08a594ac7f04aa9da2a9c93478c9e499a9
Michael Duncan <dun24011@byui.edu>
2024-11-05 16:40:39 -0700

    Prep 1-3 Finish

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage? ");
+        string percent_string = Console.ReadLine();
+        string letter = "";
+        int percent = int.Parse(percent_string);
+        if (percent >= 90)
+            {letter = "A";}
+        else if (percent >= 80)
+            {letter = "B";}
+        else if (percent >= 70)
+            {letter = "C";}
+        else if (percent >= 60)
+            {letter = "D";}
+        else if (percent < 60)
+            {letter = "F";}
+        Console.WriteLine($"Your grade is a {letter}.");
+        if (percent >= 70)
+            {Console.WriteLine("You passed!");}
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string guess_string = "";
+        int guess = 0;
+        int magic = randomGenerator.Next(1,100);
+        string direction = "";
+        int guess_amount = 0;
+        string response = "yes";
+        Console.WriteLine($"{magic}");
+        while (response == "yes")
+            {
+            while (guess != magic)
+                {Console.Write("Enter guess: ");
+                guess_string = Console.ReadLine();
+                guess = int.Parse(guess_string);
+                guess_amount = guess_amount + 1;
+                if (guess > magic)
+                    {direction = "lower";
+                    Console.WriteLine($"{direction}");}
+                else if (guess < magic)
+                    {direction = "higher";
+                    Console.WriteLine($"{direction}");}
+                else if (guess == magic)
+                    {Console.WriteLine($"You guessed it!");
+                    Console.WriteLine($"{guess_amount} guesses.");}
+                }
+            Console.Write("Do you want to continue? ");    
+            response = Console.ReadLine();
+            }
     }
 }
