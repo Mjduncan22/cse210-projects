7ce1533d5f9be2366821b1151fdabd535f613793
Michael Duncan <dun24011@byui.edu>
2024-12-04 18:37:50 -0700

    Finished

---
+++ prepare/Learning05/MathAssignment.cs
@@ -0,0 +1,14 @@
+public class MathAssignment : Assignment
+{
+    private string _section;
+    private string _problems;
+    public MathAssignment(string studentName, string topic, string section, string problems) : base(studentName, topic)
+    {
+        _section = section;
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"{GetStudentName()} - {GetTopic()}\nSection: {_section} - Problems:{_problems}";
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Assignment _assignment = new Assignment("Mary","Math");
+        // string summary = _assignment.GetSummary();
+
+        // MathAssignment _mathAssignment = new MathAssignment("Mary","Math","7.2","11-19");
+        // string summaryMath = _mathAssignment.GetHomeworkList();
+        // Console.WriteLine($"{summaryMath}");
+
+        WritingAssignment _writingAssignment = new WritingAssignment("Mary","Reading","A Sad Book");
+        string summaryWriting = _writingAssignment.GetWritingInformation();        
+        Console.WriteLine($"{summaryWriting}");
     }
 }
